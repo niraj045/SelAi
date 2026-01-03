d3cbdf98f5da23d3221059fcb2ab5000e345cd74
niraj045 <nseditingzone9@gmail.com>
2026-01-03 16:30:14 +0530

    Initial SelAi backend microservices setup

---
+++ orchestration-service/src/test/java/com/ns/selai/orchestration/OrchestrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ns.selai.orchestration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrchestrationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
